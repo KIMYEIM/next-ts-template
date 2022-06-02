b4ca2dc7e724a83412509f8cf559df02dae55ee6
KIMYEIM <kyaeim@gmail.com>
2022-06-02 23:41:27 +0900

    🎉 프로젝트 셋업

---
+++ README.md
@@ -0,0 +1,14 @@
+## next-ts-template
+
+### 사용한 기술
+
+- 코어: React, TypeScript, Next.js
+- 스타일링: Emotion
+- 린터: ESlint
+- 포맷터: Prettier
+
+### 사용하는 방법
+
+```shell
+yarn install
+```
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => (
+  <div className={styles.container}>Hello, world!</div>
+);
+
+export default Home;
